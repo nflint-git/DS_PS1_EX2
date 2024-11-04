1b7e6ad5e6f12ec94ccc4fbef2404ff925b2ded3
nflint-git <nmaf5352@gmail.com>
2024-11-04 22:01:00 +0000

    added hints

--- pys/data.py
+++ pys/data.py
@@ -1,15 +1,18 @@
 import pandas as pd
+from pandas import DataFrame
 
-def round_to_nearest_billion(x):
+
+def round_to_nearest_billion(x: float) -> int:
     return round(x / 1e9)
 
-def load_dataset(data):
+
+def load_dataset(data: str) -> DataFrame:
     """
     load the dataset from a CSV
-    
+
     """
     df = pd.read_csv(data)
-    country = ['United Kingdom', 'Japan', 'China','Germany','Switzerland']
+    country = ['United Kingdom', 'Japan', 'China', 'Germany', 'Switzerland']
     years = [str(year) for year in range(2000, 2023)]
     filtered_df = df[df['Country'].isin(country)]
     filtered_df = filtered_df[['Country'] + years]
